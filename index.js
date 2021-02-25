7de1e8c4d3cb37a408a2d7f9387b5eb87c44bf08
divana <63994691+divanaAnggita@users.noreply.github.com>
2021-02-25 08:07:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,25 @@
 const express = require("express")
 const app = express()
 const auth = require("./auth")
+const cors = require("cors")
 
+app.use(
+    cors({
+        origin: '*',
+        optionSuccessStatus: 200,
+    })
+);
+
+const siswa = require("./route/siswa")
+app.use("/siswa", auth, siswa)
+
+const pelanggaran = require("./route/pelanggaran")
+app.use("/pelanggaran", auth, pelanggaran)
 
 //base url -> http:/localhost:2000/pegawai
 //import route pegawai
 const pegawai = require("./route/pegawai")
-app.use("/pegawai",auth, pegawai)
+app.use("/pegawai", auth, pegawai)
 
 const user = require("./route/user")
 app.use("/", user)
