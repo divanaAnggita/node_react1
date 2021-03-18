28619d245f6fd9cb86dfee7d47a61a776196dc22
divana <63994691+divanaAnggita@users.noreply.github.com>
2021-03-19 00:27:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ app.use("/pegawai", auth, pegawai)
 const user = require("./route/user")
 app.use("/", user)
 
+//base url -> http://localhost:2000/pelanggaran_siswa
+const pelanggaran_siswa = require("./route/pelanggaran_siswa")
+app.use("/pelanggaran_siswa", auth, pelanggaran_siswa)
+
+//base url 
+const detail_pelanggaran_siswa = require("./route/detail_pelanggaran_siswa")
+app.use("/detail_pelanggaran_siswa", auth, detail_pelanggaran_siswa)
+
 //membuat web server dengan port 2000
 app.listen(2000, () => {
     console.log("server run on port 2000")
